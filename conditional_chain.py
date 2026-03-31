4ef53259c94dfe82a1ec17946479baf1518cd929
saranshtyagi <18saranshtyagi2004@gmail.com>
2026-03-31 17:04:32 +0530

    Refactor response prompts to include sentiment in feedback responses

--- conditional_chain.py
+++ conditional_chain.py
@@ -33,29 +33,34 @@ prompt1 = PromptTemplate(
 classifer_chain = prompt1 | model | parser2
 
 prompt2 = PromptTemplate(
-    template='Respond with an appropriate response to the user for this positive feedback: \n {feedback}',
-    input_variables = ['feedback']
+    template='Respond with an appropriate response to the user for this positive feedback: \n {feedback} \n {sentiment}',
+    input_variables = ['feedback', 'sentiment']
 )
 
 prompt3 = PromptTemplate(
-    template='Respond with an appropriate response to the user for this negative feedback: \n {feedback}', 
-    input_variables = ['feedback']
+    template='Respond with an appropriate response to the user for this negative feedback: \n {feedback} \n {sentiment}', 
+    input_variables = ['feedback', 'sentiment']
 )
 
 prompt4 = PromptTemplate(
-    template='Respond with an appropriate response to the user for this neutral feedback: \n {feedback}',
-    input_variables = ['feedback']
+    template='Respond with an appropriate response to the user for this neutral feedback: \n {feedback} \n {sentiment}',
+    input_variables = ['feedback', 'sentiment']
 )
 
 branch_chain = RunnableBranch(
-    (lambda input: input.sentiment == 'positive', prompt2 | model | parser1),
-    (lambda input: input.sentiment == 'negative', prompt3 | model | parser1), 
-    (lambda input: input.sentiment == 'neutral', prompt4 | model | parser1), 
+    (lambda input: input["sentiment"] == 'positive', prompt2 | model | parser1),
+    (lambda input: input["sentiment"] == 'negative', prompt3 | model | parser1), 
+    (lambda input: input["sentiment"] == 'neutral', prompt4 | model | parser1), 
     RunnableLambda(lambda input: "Invalid sentiment")
 )
 
-chain = classifer_chain | branch_chain
+chain = (
+    lambda x: {
+        "feedback": x["feedback"],
+        "sentiment": classifer_chain.invoke(x).sentiment
+    }
+) | branch_chain
 
-result = chain.invoke({'feedback': 'The product quality is excellent and I am very satisfied with my purchase!'})
+result = chain.invoke({'feedback': 'The product quality is terrible and I am very dissatisfied with my purchase!'})
 
 print(result)
